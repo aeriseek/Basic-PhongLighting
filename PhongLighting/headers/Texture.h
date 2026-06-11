cb8c5ba34d5090aa2846ad61cac3d413167c69d9
AErisee <mondonyu@gmail.com>
2026-06-11 17:04:09 +0300

    add mesh, camera classes

--- PhongLighting/headers/Texture.h
+++ PhongLighting/headers/Texture.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 #include <glad/glad.h>
 #include <stb_image.h>
-
+#include "Shader.h"
 
 class Texture
 {
@@ -10,7 +10,7 @@ public:
 	const char* type;
 	GLuint unit;
 	Texture(const char* imagePath, const char* type, GLuint slot);
-
+	void TexUnit(Shader& shader, const char* uniform, GLuint unit);
 	void Bind();
 	void Unbind();
 	void Delete();
