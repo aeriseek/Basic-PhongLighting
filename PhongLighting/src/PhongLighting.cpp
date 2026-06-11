53a58d008c1cf2f5dc35f5591bed461c5d905a47
AErisee <mondonyu@gmail.com>
2026-06-11 15:50:21 +0300

    add basic template

--- PhongLighting/src/PhongLighting.cpp
+++ PhongLighting/src/PhongLighting.cpp
@@ -1,12 +1,41 @@
 ﻿// PhongLighting.cpp : Defines the entry point for the application.
 //
 
-#include "PhongLighting.h"
-
-using namespace std;
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	const int width = 900;
+	const int height = 900;
+
+	GLFWwindow* win = glfwCreateWindow(width, height, "Basic Phong Lighting", nullptr, nullptr);
+	glfwSetWindowPos(win, 300, 300);
+	if (win == nullptr)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(win);
+	gladLoadGL();
+
+	glViewport(0, 0, width, height);
+
+
+	while (!glfwWindowShouldClose(win))
+	{
+
+
+
+
+		glfwPollEvents();
+	}
+
 	return 0;
 }
